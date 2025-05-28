9d0939adb0100d150477e0fcf05b91f11233fb73
SamuelHerrera-blip <samuel.herrera.martinez@gmail.com>
2025-05-28 14:13:38 -0500

    index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
-// Recibir el JSON del webhook
+// Recibir el JSON
 $input = json_decode(file_get_contents('php://input'), true);
 
-// Guardar log en archivo local
+// Guardar log en archivo local (opcional)
 file_put_contents("log.txt", json_encode($input, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND);
 
-// Mostrar debug para Render
+// Mostrar contenido recibido como respuesta
 echo json_encode([
-    'status' => 'ok',
-    'debug' => $input
+    'content' => "🤖 Hola, soy el bot de Faster Cash. ¿En qué puedo ayudarte? \nEscribe: 'ya soy cliente' o 'todavía no soy cliente'",
 ]);
 exit;
